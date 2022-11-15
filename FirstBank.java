b826cf9612cdccae1a46bdae093e8ec247cad9a2
Mohamed Dahir Saney <106882978+MohamedDahir21@users.noreply.github.com>
2022-11-15 08:12:05 +0300

    Add files via upload

---
+++ FirstBank.java
@@ -0,0 +1,15 @@
+public class FirstBank {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Account tim = new Account("Tim Statler", "A00001");
+
+
+        Account beyonce = new Account("Beyonce", "B00002");
+        beyonce.showMenu();
+
+
+    }
+
+}
